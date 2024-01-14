24d6086087f6b373b2c255f8576ffedeaaa62c6b
William Rutkowski <budski82@users.noreply.github.com>
2024-01-14 16:44:14 -0500

    ww

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 import express from 'express'
-
 const app = express()
+
+// socket.io setup
+import http from 'http'
+
+
 const port = 3000
 
 app.use(express.static('public'));
-
 app.get('/', (req,res)=>{
 	res.sendFile(__dirname + '/index.html')
 })
+
+
+
 
 app.listen(port, ()=>{
 	console.log(`example app listening on port ${port}`);
