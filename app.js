f58bb587cd2ef6b5e496afa50ced02c89ea08493
William Rutkowski <budski82@users.noreply.github.com>
2024-01-14 17:08:33 -0500

    Update app.js

--- app.js
+++ app.js
@@ -18,6 +18,18 @@ app.get('/', (req,res)=>{
 
 const backEndPlayers     = {}
 const backEndProjectiles = {}
+const SPEED = 5
+const RADIUS = 10
+const PROJECTILE_RADIUS = 5
+let projectileId = 0
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+
+  io.emit('updatePlayers', backEndPlayers)
+
+  socket.on('shoot', ({ x, y, angle }) => {
+    projectileId++
 
 app.listen(port, ()=>{
 	console.log(`example app listening on port ${port}`);
